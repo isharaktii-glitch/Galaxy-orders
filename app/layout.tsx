8ed4862bc75cff79f428a655b3c37ca5264e1a19
Iahara <isharaktii@gmail.com>
2026-07-01 10:26:03 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Toaster } from 'react-hot-toast';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Galaxy Mart | Tagla Market',
+  description: 'Galaxy Workers Multi-Vendor Marketplace',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Toaster position="top-right" />
+        {children}
+      </body>
+    </html>
+  );
+}
